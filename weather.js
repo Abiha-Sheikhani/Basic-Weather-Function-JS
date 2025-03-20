cd3d63a14035367841b829e2ceb5904584e06fb9
Abiha-Sheikhani <abihajawed345@gmail.com>
2025-03-20 23:44:20 +0500

    first commit

---
+++ weather.js
@@ -0,0 +1,7 @@
+let typeweatherdegree = +prompt("Enter your area temperature in degree")
+
+if(typeweatherdegree >=45){
+    document.write(
+        '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ4ruPSaPLirLfjY63cozJQt8nX5yLVNbsfw&s" class="hot mt-5 d-flex">'
+    )
+}
