11a89b348e65edb7f1428d16b5ad653ddd9be4d4
Abiha-Sheikhani <abihajawed345@gmail.com>
2025-04-25 15:38:40 +0500

    all done

--- weather.js
+++ weather.js
@@ -1,7 +1,141 @@
-let typeweatherdegree = +prompt("Enter your area temperature in degree")
+let typeweatherdegree = prompt("Enter your city name to find the Temperature of your city.... \n you can write\n karachi, lahore, islamabad, rawalpindi, peshawar, multan & quetta ")
 
-if(typeweatherdegree >=45){
+if (typeweatherdegree === "karachi") {
     document.write(
-        '<img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJ4ruPSaPLirLfjY63cozJQt8nX5yLVNbsfw&s" class="hot mt-5 d-flex">'
+        "<div class=' d-flex justify-content-center p-5 mycard'>" +
+            "<div class='card animate__animated animate__rotateIn' style='width: 25rem;'>" +
+            "<img src='./images/karachi.PNG' class='card-img-top' alt='picture' style='height: 15rem;'>" +
+            "<div class='card-body'>" +
+            "<h2 class='card-title text-center'>" + "Karachi" + "</h2>" +
+            " <p class='card-text'>" + "The average temperature of karachi now days is between 25 to 30 degrees." + "</p>" +
+            " <h5>" + "Recommendations for Activities:" + "</h5>" +
+            " <p>" + "Stay Hydrated: Due to the rising temperatures, it's essential to drink plenty of water throughout the day to stay hydrated​ and many more....." +
+            "</p>" +
+            "<h5>" + "Seasonal Foods to Enjoy:" + "</h5>" +
+            "<p>" + "Leafy Greens: Vegetables like lettuce, Swiss chard, and celery are abundant during this season and are excellent for salads and light meals and many more......"
+            + "</p>" +
+            "</div>" +
+            "</div>"
+            + " </div>"
     )
+   
+}
+else if( typeweatherdegree === "lahore"){
+    document.write(
+        "<div class=' d-flex justify-content-center p-5 mycard'>" +
+            "<div class='card animate__animated animate__rotateIn' style='width: 25rem;'>" +
+            "<img src='./images/lahore.PNG' class='card-img-top' alt='picture' style='height: 15rem;'>" +
+            "<div class='card-body'>" +
+            "<h2 class='card-title text-center'>" + "Lahore" + "</h2>" +
+            " <p class='card-text'>" + "The average temperature of karachi now days is between 23 to 28 degrees." + "</p>" +
+            " <h5>" + "Recommendations for Activities:" + "</h5>" +
+            " <p>" + "The sun is getting stronger, so wearing light cotton clothes, sunglasses, and sunscreen is a good idea for daytime outings​ and many more....." +
+            "</p>" +
+            "<h5>" + "Seasonal Foods to Enjoy:" + "</h5>" +
+            "<p>" + "Lahori Nashta – Enjoy a classic halwa puri with chana and lassi, a local favorite for a refreshing start to the day and many more......"
+            + "</p>" +
+            "</div>" +
+            "</div>"
+            + " </div>"
+    )
+}
+else if( typeweatherdegree === "islamabad"){
+    document.write(
+        "<div class=' d-flex justify-content-center p-5 mycard'>" +
+            "<div class='card animate__animated animate__rotateIn' style='width: 25rem;'>" +
+            "<img src='./images/islamabad.PNG' class='card-img-top' alt='picture' style='height: 15rem;'>" +
+            "<div class='card-body'>" +
+            "<h2 class='card-title text-center'>" + "Islamabad" + "</h2>" +
+            " <p class='card-text'>" + "The average temperature of karachi now days is between 20 to 23 degrees." + "</p>" +
+            " <h5>" + "Recommendations for Activities:" + "</h5>" +
+            " <p>" + "The sun is getting stronger, so wearing light cotton clothes, sunglasses, and sunscreen is a good idea for daytime outings​ and many more....." +
+            "</p>" +
+            "<h5>" + "Seasonal Foods to Enjoy:" + "</h5>" +
+            "<p>" + "Lahori Nashta – Enjoy a classic halwa puri with chana and lassi, a local favorite for a refreshing start to the day and many more......"
+            + "</p>" +
+            "</div>" +
+            "</div>"
+            + " </div>"
+    )
+}
+else if( typeweatherdegree === "multan"){
+    document.write(
+        "<div class=' d-flex justify-content-center p-5 mycard'>" +
+            "<div class='card animate__animated animate__rotateIn' style='width: 25rem;'>" +
+            "<img src='./images/multan.PNG' class='card-img-top' alt='picture' style='height: 15rem;'>" +
+            "<div class='card-body'>" +
+            "<h2 class='card-title text-center'>" + "Multan" + "</h2>" +
+            " <p class='card-text'>" + "The average temperature of karachi now days is between 15 to 18 degrees." + "</p>" +
+            " <h5>" + "Recommendations for Activities:" + "</h5>" +
+            " <p>" + "The sun is getting stronger, so wearing light cotton clothes, sunglasses, and sunscreen is a good idea for daytime outings​ and many more....." +
+            "</p>" +
+            "<h5>" + "Seasonal Foods to Enjoy:" + "</h5>" +
+            "<p>" + "Lahori Nashta – Enjoy a classic halwa puri with chana and lassi, a local favorite for a refreshing start to the day and many more......"
+            + "</p>" +
+            "</div>" +
+            "</div>"
+            + " </div>"
+    )
+}
+else if( typeweatherdegree === "peshawar"){
+    document.write(
+        "<div class=' d-flex justify-content-center p-5 mycard'>" +
+            "<div class='card animate__animated animate__rotateIn' style='width: 25rem;'>" +
+            "<img src='./images/peshawar.PNG' class='card-img-top' alt='picture' style='height: 15rem;'>" +
+            "<div class='card-body'>" +
+            "<h2 class='card-title text-center'>" + "peshawar" + "</h2>" +
+            " <p class='card-text'>" + "The average temperature of karachi now days is between 23 to 28 degrees." + "</p>" +
+            " <h5>" + "Recommendations for Activities:" + "</h5>" +
+            " <p>" + "The sun is getting stronger, so wearing light cotton clothes, sunglasses, and sunscreen is a good idea for daytime outings​ and many more....." +
+            "</p>" +
+            "<h5>" + "Seasonal Foods to Enjoy:" + "</h5>" +
+            "<p>" + "Lahori Nashta – Enjoy a classic halwa puri with chana and lassi, a local favorite for a refreshing start to the day and many more......"
+            + "</p>" +
+            "</div>" +
+            "</div>"
+            + " </div>"
+    )
+}
+else if( typeweatherdegree === "quetta"){
+    document.write(
+        "<div class=' d-flex justify-content-center p-5 mycard'>" +
+            "<div class='card animate__animated animate__rotateIn' style='width: 25rem;'>" +
+            "<img src='./images/quetta.PNG' class='card-img-top' alt='picture' style='height: 15rem;'>" +
+            "<div class='card-body'>" +
+            "<h2 class='card-title text-center'>" + "Quetta" + "</h2>" +
+            " <p class='card-text'>" + "The average temperature of karachi now days is between 16 to 20 degrees." + "</p>" +
+            " <h5>" + "Recommendations for Activities:" + "</h5>" +
+            " <p>" + "The sun is getting stronger, so wearing light cotton clothes, sunglasses, and sunscreen is a good idea for daytime outings​ and many more....." +
+            "</p>" +
+            "<h5>" + "Seasonal Foods to Enjoy:" + "</h5>" +
+            "<p>" + "Lahori Nashta – Enjoy a classic halwa puri with chana and lassi, a local favorite for a refreshing start to the day and many more......"
+            + "</p>" +
+            "</div>" +
+            "</div>"
+            + " </div>"
+    )
+}
+else if( typeweatherdegree === "rawalpindi"){
+    document.write(
+        "<div class=' d-flex justify-content-center p-5 mycard'>" +
+            "<div class='card animate__animated animate__rotateIn' style='width: 25rem;'>" +
+            "<img src='./images/rawalpindi.PNG' class='card-img-top' alt='picture' style='height: 15rem;'>" +
+            "<div class='card-body'>" +
+            "<h2 class='card-title text-center'>" + "Rawalpindi" + "</h2>" +
+            " <p class='card-text'>" + "The average temperature of karachi now days is between 23 to 28 degrees." + "</p>" +
+            " <h5>" + "Recommendations for Activities:" + "</h5>" +
+            " <p>" + "The sun is getting stronger, so wearing light cotton clothes, sunglasses, and sunscreen is a good idea for daytime outings​ and many more....." +
+            "</p>" +
+            "<h5>" + "Seasonal Foods to Enjoy:" + "</h5>" +
+            "<p>" + "Lahori Nashta – Enjoy a classic halwa puri with chana and lassi, a local favorite for a refreshing start to the day and many more......"
+            + "</p>" +
+            "</div>" +
+            "</div>"
+            + " </div>"
+    )
+}
+else{
+    document.write(
+    "<div class='animate__animated animate__slideInDown animate__slow'>"+ "<img src='./images/tryagain.jpg' alt=''>" +"</div>"
+)
 }
